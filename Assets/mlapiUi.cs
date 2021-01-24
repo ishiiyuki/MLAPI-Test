5d0dcc47680e8cfeb174155a0723f3030ddd5d04
ishiiyuki <mizuki.yuuri@gmail.com>
2021-01-25 01:21:01 +0900

    add project

---
+++ Assets/mlapiUi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mlapiUi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnClickStartHost()
+    {
+        Debug.Log("Start Host!!");
+        MLAPI.NetworkingManager.Singleton.StartHost();
+    }
+
+    public void OnClickStartClient()
+    {
+        Debug.Log("Start Client!!");
+        MLAPI.NetworkingManager.Singleton.StartClient();
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MLAPI.NetworkedBehaviour
+{
+
+    float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (this.IsOwner)
+        {
+            float x = Input.GetAxis("Horizontal") * speed;
+            float z = Input.GetAxis("Vertical") * speed;
+            this.transform.position += new Vector3(x, 0, z);
+        }
+    }
+}
